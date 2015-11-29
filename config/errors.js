4473d2cdb6c8fc6b267532b96f5a47f802f44aba
Travis Webb <me@traviswebb.com>
2015-11-29 14:10:25 -0500

    [config] added errors config

--- config/errors.js
+++ config/errors.js
@@ -5,11 +5,11 @@ module.exports = {
     statusCode: 404
   },
   'Footprint.UnknownParentModel': {
-    message: 'The Parent Model specified in this association request is not defined in the API',,
+    message: 'The Parent Model specified in this association request is not defined in the API',
     statusCode: 404
   },
   'Footprint.UnknownChildModel': {
-    message: 'The Child Model specified in this association request is not defined in the API',,
+    message: 'The Child Model specified in this association request is not defined in the API',
     statusCode: 404
   }
 

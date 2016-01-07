2fa07dd66ec642a8a5632e4f578bd2881570cb41
Travis Webb <me@traviswebb.com>
2016-01-07 01:44:55 -0500

    [tests] update route id generation, include tests

---
+++ test/lib/footprints.test.js
@@ -0,0 +1,40 @@
+const assert = require('assert')
+const _ = require('lodash')
+const lib = require('../../lib')
+
+describe('lib.Footprints', () => {
+  describe('#getRouteIdFromHandler', () => {
+    it('should generate a correct route id from an internal "handler" object', () => {
+      const expectedRouteId = '[footprint] /info -> DefaultController.info'
+      const handlerObject = {
+        controllerName: 'DefaultController',
+        handlerName: 'info'
+      }
+
+      const id = lib.Footprints.getRouteIdFromHandler(
+        handlerObject, 'footprint', global.app.config)
+
+      assert.equal(id, expectedRouteId)
+    })
+    it('should include prefix, if specified', () => {
+      const expectedRouteId = '[footprint] /123/info -> DefaultController.info'
+      const handlerObject = {
+        controllerName: 'DefaultController',
+        handlerName: 'info'
+      }
+      const config = _.defaultsDeep({ footprints: { prefix: '123' } }, global.app.config)
+      const id = lib.Footprints.getRouteIdFromHandler(handlerObject, 'footprint', config)
+
+      assert.equal(id, expectedRouteId)
+    })
+  })
+  describe.skip('#getRouteIdFromRoute', () => {
+    it('should generate a route id from a hapijs route object', () => {
+
+    })
+    it('should generate unique ids for similar routes', () => {
+
+    })
+
+  })
+})

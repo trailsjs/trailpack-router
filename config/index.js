514e7b55b5a6a8bea968bb6d672ccdf580a15252
Travis Webb <me@traviswebb.com>
2016-02-07 18:11:18 -0500

    [cleanup] remove vestigial config files

--- config/index.js
+++ config/index.js
@@ -1,4 +1,2 @@
-exports.errors = require('./errors')
-exports.footprints = require('./footprints')
 exports.policies = require('./policies')
 exports.trailpack = require('./trailpack')
--- config/policies.js
+++ config/policies.js
@@ -1,12 +1,10 @@
 module.exports = {
   FootprintController: {
     create: [ 'FootprintPolicy.create' ],
-    createWithId: [ 'FootprintPolicy.createWithId' ],
     find: [ 'FootprintPolicy.find' ],
     update: [ 'FootprintPolicy.update' ],
     destroy: [ 'FootprintPolicy.destroy' ],
     createAssociation: [ 'FootprintPolicy.createAssociation' ],
-    createAssociationWithId: [ 'FootprintPolicy.createAssociationWithId' ],
     findAssociation: [ 'FootprintPolicy.findAssociation' ],
     updateAssociation: [ 'FootprintPolicy.updateAssociation' ],
     destroyAssociation: [ 'FootprintPolicy.destroyAssociation' ]
